9872339dd5d24fcadcb7b46afc22a4fe9ccab4ce
tkhshtsh0917 <48384135+tkhshtsh0917@users.noreply.github.com>
2022-03-26 22:50:52 +0900

    feat: Chapter04

---
+++ ch04/mult/Mult.asm
@@ -0,0 +1,44 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Mult.asm
+
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+//
+// This program only needs to handle arguments that satisfy
+// R0 >= 0, R1 >= 0, and R0*R1 < 32768.
+
+// Put your code here.
+
+    // initialize
+    @counter
+    M=0
+    @R2
+    M=0
+
+(LOOP)
+    // GOTO END if counter - R1 >= 0
+    @counter
+    D=M
+    @R1
+    D=D-M
+    @END
+    D;JGE
+
+    // R2 += R0
+    @R0
+    D=M
+    @R2
+    M=D+M
+
+    // counter += 1
+    @counter
+    M=M+1
+
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
